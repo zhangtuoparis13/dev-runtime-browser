0599d4b48fd85322b6634ebc26773c4d2409de1a
Vitor Silva <vitormsilva@users.noreply.github.com>
2015-10-22 17:07:09 +0100

    Update runtime-browser.js

--- src/runtime-browser.js
+++ src/runtime-browser.js
@@ -5,12 +5,3 @@ var sandboxFactoryBrowser = new SandboxFactoryBrowser();
 
 var runtime = new RuntimeUA(sandboxFactoryBrowser);
 window.runtime = runtime;
-
-var loadHyperty = runtime.loadHyperty('http://localhost:4000/dist/VertxProtoStub.js');
-var loadStub = runtime.loadStub('hyperty-runtime://sp1/protostub/123');
-
-Promise.all([loadHyperty, loadStub]).then(function(result) {
-  console.log(resolved);
-}).catch(function(reason) {
-  console.log('error:', reason);
-});
