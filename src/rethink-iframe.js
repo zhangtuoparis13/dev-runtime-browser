8b3be5168dcaab394c1839e8a0c8f1359070820a
anton.roman <anton.roman@quobis.com>
2015-11-26 09:41:10 +0100

    initial version

---
+++ src/rethink-iframe.js
@@ -0,0 +1,14 @@
+/**
+* main reTHINK for browser runtime
+*/
+
+/*
+  This code is under development
+*/
+
+
+//import {RuntimeUA, Sandbox} from 'runtime-core';
+
+console.log('rethink-iframe created');
+//console.log('Runtime: ', RuntimeUA);
+//console.log('Sandbox: ', Sandbox);
--- src/rethink.js
+++ src/rethink.js
@@ -1,10 +1,27 @@
-/**
-* main reTHINK for browser runtime
-*/
+/*Creating rethink iFrame*/
 
-import {RuntimeUA, Sandbox} from 'runtime-core';
+  try{ 
 
-console.log('Runtime: ', RuntimeUA);
-console.log('Sandbox: ', Sandbox);
+    let rethinkIframe = document.getElementById('rethink-iframe');
 
+      if (!rethinkIframe) {
+        rethinkIframe = document.createElement('iframe');
+        rethinkIframe.setAttribute('id', 'rethink-iframe');
+        rethinkIframe.setAttribute('seamless', '');
+        rethinkIframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
+
+        //rethinkIframe.style.display = 'none';
+        document.querySelector('body').appendChild(rethinkIframe);
+
+        // Instantiate the application inside the AppSandbox
+        var script = document.createElement('script');
+        script.type = 'text/JavaScript';
+        script.src = 'rethink-iframe.js';
+
+        sandbox.contentWindow.document.getElementsByTagName('body')[0].appendChild(script);
+      }
+
+    } catch (e) {
+      throw new Error('Your environment does not allow the iframe deployment:  \n', e);
+    }
 
