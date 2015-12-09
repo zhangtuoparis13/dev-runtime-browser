89cc28574f8644fc2ae282f229b2cddc7be93305
David <dvilchez@xuaps.com>
2015-12-09 11:46:37 +0100

    Move Sandbox class to a separate file

---
+++ src/SandboxBrowser.js
@@ -0,0 +1,20 @@
+import { Sandbox } from 'runtime-core';
+
+const SANDBOX_FILE = 'Sandbox.js';
+export default class SandBoxWebWorker extends Sandbox{
+   constructor(){
+     super();
+     if(!!Worker){
+         this._worker = new Worker(SANDBOX_FILE);
+         this._worker.addEventListener('message', function(e){
+             this._onMessage(e.data);
+         }.bind(this));
+     }else{
+         throw new Error('Your environment does not support worker \n', e);
+     }
+   }
+
+   _onPostMessage(msg){
+       this._worker.postMessage(msg);
+   }
+}
