1711d49212be29376e2b1d86af680be4cd6cf30f
David <dvilchez@xuaps.com>
2015-12-09 11:21:08 +0100

    Implement external side of Sandbox
    Why:
    
    We need isolate hyperties/protostub/core in their own worker and
    communicate with them components through messaging
    
    This change addresses the need by:
    
    Add createSandbox factory method
    Add SandBoxBrowser class that extends Sandbox
    Pass message from outside to worker and vice versa

--- src/SandBoxFactory.js
+++ src/SandBoxFactory.js
@@ -1,7 +1,27 @@
-const SANDBOX_FILE = 'Sandbox.js'
+/*jshint esnext: true */
+import { Sandbox } from 'runtime-core';
+const SANDBOX_FILE = 'Sandbox.js';
 
+class SandBoxWebWorker extends Sandbox{
+   constructor(){
+     super();
+     if(!!Worker){
+         this._worker = new Worker(SANDBOX_FILE);
+         this._worker.addEventListener('message', function(e){
+             this._onMessage(e.data);
+         }.bind(this));
+     }else{
+         throw new Error('Your environment does not support worker \n', e);
+     }
+   }
+
+   _onPostMessage(msg){
+       this._worker.postMessage(msg);
+   }
+}
+      
 function createSandbox(){
-    return new Worker(SANDBOX_FILE)
+    return new SandBoxWebWorker();
 }
 
-export default { createSandbox }
+export default { createSandbox };
--- test/SandBoxBrowser.spec.js
+++ test/SandBoxBrowser.spec.js
@@ -1,12 +1,46 @@
-import { expect } from 'chai'
-import sandboxFactory from '../src/SandBoxFactory'
+/*jshint esnext: true */
+import { expect } from 'chai';
+import sandboxFactory from '../src/SandBoxFactory';
+
+let init, post, sendMessage;
+
+window.Worker = function(){
+    init = true;
+    return {
+        addEventListener: function(msg, callback) {
+                            sendMessage = callback; 
+                          },
+        postMessage: function(msg) {
+            post = true;
+        }
+    };
+};
 
 describe('SandBoxBrowser', function(){
-    describe('create sandbox browser', function(){
-        it('should return a web worker object', function(){
-            let sandbox = sandboxFactory.createSandbox()
+    beforeEach(function(){
+        init = false;
+        post = false;
+        sendMessage = undefined;
+    });
 
-            expect(sandbox).to.be.a('Worker')
-        })
-    })
-})
+    it('should instantiate a web worker object', function(){
+        let sandbox = sandboxFactory.createSandbox();
+
+        expect(init).to.be.true;
+    });
+
+    it('should send outside messages to web worker', function(){
+        let sandbox = sandboxFactory.createSandbox();
+        
+        sandbox.postMessage({header:{}});
+        expect(post).to.be.true;
+    });
+
+    it('should receive inside messages from web worker', function(done){
+        let sandbox = sandboxFactory.createSandbox();
+        sandbox.addListener('ua.me/mock', function(msg){
+            done();
+        });
+        sendMessage({data:{header: {to:'ua.me/mock'}}});
+    });
+});
