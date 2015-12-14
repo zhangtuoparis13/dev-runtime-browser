200f8e323e4e7ec3de8cfcc918c263718156058f
David <dvilchez@xuaps.com>
2015-12-14 17:18:30 +0100

    Rename some components

--- test/SandboxBrowserIntegration.spec.js
+++ test/SandboxBrowserIntegration.spec.js
@@ -4,7 +4,7 @@ import Sandbox from '../src/SandboxBrowser';
 describe('SandboxBrowser Integration Tests', function(){
     it('should manage messages with internal components - send and receive', function(done){
         this.timeout(0);
-        var sandbox = new Sandbox('base/test/resources/environment.js');
+        var sandbox = new Sandbox('base/test/resources/serviceprovider.js');
 
         setTimeout(()=>{
             sandbox.postMessage({to:'sandbox://internal',type:'create', body: { config:'', url: 'ua.me', sourceCode: '(function (url, miniBus, config){ 2+2; })'} }, function(msg){
@@ -15,7 +15,18 @@ describe('SandboxBrowser Integration Tests', function(){
 
     it('should create components', function(done){
         this.timeout(0);
-        var sandbox = new Sandbox('base/test/resources/environment.js');
+        var sandbox = new Sandbox('base/test/resources/serviceprovider.js');
+        sandbox.addListener('ua.me', function(e){
+            done();
+        });
+        setTimeout(()=>{
+            sandbox.postMessage({to:'sandbox://internal',type:'create', body: { config:'', url: 'ua.me', sourceCode: '(function (url, miniBus, config){ self.postMessage({to:"ua.me"}); })'}} );
+        }, 1200);
+    });
+
+    it('should load RuntimeUA context', function(){
+        this.timeout(0);
+        var sandbox = new Sandbox('base/test/resources/runtimeua.js');
         sandbox.addListener('ua.me', function(e){
             done();
         });
