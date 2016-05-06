35ba3b3f84e7011135e7abc0734bfcab88e2a914
David Vílchez <dvilchez@xuaps.com>
2016-05-06 12:55:50 +0200

    Add name to iframe

--- src/iframe.js
+++ src/iframe.js
@@ -23,6 +23,7 @@
 export function create(src){
     var iframe = document.createElement('iframe');
     iframe.setAttribute('id', 'rethink');
+    iframe.setAttribute('name', 'rethink');
     iframe.setAttribute('seamless', '');
     iframe.setAttribute('src', src);
     iframe.setAttribute('sandbox', 'allow-forms allow-scripts allow-same-origin allow-popups');
